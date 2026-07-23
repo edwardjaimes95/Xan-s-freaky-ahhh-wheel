54d5f57fc5061bf04adba368435267f5e23bd2e2
edwardjaimes95 <eddiejaimes95@gmail.com>
2026-07-23 09:34:33 -0700

    Add files via upload

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-const CACHE='xans-wheel-v4-3-shared';
+const CACHE='xans-wheel-v4-4-inventory';
 const FILES=["./", "./index.html", "./manifest.webmanifest","./wheel-options.json", "./icons/icon-192.png", "./icons/icon-512.png", "./assets/rewards/cuddle.png", "./assets/rewards/snack.png", "./assets/rewards/movie.png", "./assets/rewards/massage.png", "./assets/rewards/date.png", "./assets/rewards/mystery.png"];
 self.addEventListener('install',e=>e.waitUntil(caches.open(CACHE).then(c=>c.addAll(FILES))));
 self.addEventListener('activate',e=>e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k))))));
